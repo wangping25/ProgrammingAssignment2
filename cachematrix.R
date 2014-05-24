11ddff3e6936da45aadb06028f5cebadf59cf98f
Ping Wang <ping@graphlab.com>
2014-05-23 20:26:00 -0700

    implement cache solve

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m_inverse <- NULL
+  set <- function(y) {
+          x <<- y
+          m_inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m_inverse <<- inverse
+  getInverse <- function() m_inverse
+  list(set = set, get = get,
+      setInverse = setInverse,
+      getInverse = getInverse)
 }
 
 
@@ -12,4 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+    }
+    data <- x$get()
+    x$setInverse(solve(data, ...))
+    x$getInverse()
 }
